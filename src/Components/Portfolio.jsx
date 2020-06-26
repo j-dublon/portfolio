8d604b4041bcba397ff600dc9c5138b3e44bde30
j-dublon <j.johnson.dublon@gmail.com>
2020-06-26 10:59:36 +0100

    remove comments

--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -77,22 +77,3 @@ class Portfolio extends Component {
 }
 
 export default Portfolio;
-
-//   <img src={require("../Images/NNsmall.jpg")} alt="Northcoders News" />
-//   <a href={"https://j-dublon-react-news-app.netlify.app/"}>
-//     Northcoders News
-//   </a>
-
-// <h3>
-//   <img src={require("../Images/APIsmall.jpg")} alt="RESTful API" />
-//   <a href={"https://j-dublon-nc-news.herokuapp.com/api"}>RESTful API</a>
-// </h3>
-// <h3>
-//   <img
-//     src={require("../Images/AMHsmall.jpg")}
-//     alt="Alien Matter Hunt"
-//   />
-//   <a href={"https://github.com/j-dublon/alien-matter-hunt"}>
-//     Alien Matter Hunt
-//   </a>
-// </h3>
