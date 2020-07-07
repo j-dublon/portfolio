5eb1c9a75541bcc84bcb24ce6fa363d88c7b6214
j-dublon <j.johnson.dublon@gmail.com>
2020-07-07 08:58:24 +0100

    try jpg image on github

--- src/Components/Image.jsx
+++ src/Components/Image.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Image = () => {
   return (
     <img
-      src={require("../Images/linkedin.png")}
+      src={require("../Images/linkedin.jpg")}
       alt="screenshot"
       className="linkedin"
     />
